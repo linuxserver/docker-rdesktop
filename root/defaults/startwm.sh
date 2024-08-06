ea4a247e86523cf846d39c8eb8bc1a8fbbaf9a0e
thelamer <ryankuba@gmail.com>
2024-08-06 17:47:19 -0400

    refresh ubuntu-xfce

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
+
+# Combine env
+/usr/bin/with-contenv /usr/bin/env | sed 's/^/export /g' > /defaults/env.sh
+source /defaults/env.sh
+rm /defaults/env.sh
+
+# Enable Nvidia GPU support if detected
+if which nvidia-smi; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Start Pulseaudio
 /startpulse.sh &
+
+# Disable compositing
 setterm blank 0
 setterm powerdown 0
-if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
-  sed -i \
-    '/use_compositing/c <property name="use_compositing" type="bool" value="false"/>' \
-    "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+if [ ! -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
+  mkdir -p "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/
+cat <<EOT >> "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<channel name="xfwm4" version="1.0">
+  <property name="general" type="empty">
+    <property name="use_compositing" type="bool" value="false"/>
+  </property>
+</channel>
+EOT
 fi
+
+# Launch DE
 /usr/bin/xfce4-session > /dev/null 2>&1
