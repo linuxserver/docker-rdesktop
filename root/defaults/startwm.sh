493e93bde3838619066fb0f4697dc36d4d2c27f8
thelamer <ryankuba@gmail.com>
2024-08-06 17:28:50 -0400

    refresh alpine-i3

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+
+# Combine env
+/usr/bin/with-contenv /usr/bin/env | sed 's/^/export /g' > /defaults/env.sh
+source /defaults/env.sh
+rm /defaults/env.sh
+
+# Enable Nvidia GPU support if detected
+if which nvidia-smi; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Start Pulseaudio
 /startpulse.sh &
-setterm blank 0
-setterm powerdown 0
+
+# Launch DE
 /usr/bin/i3 > /dev/null 2>&1
