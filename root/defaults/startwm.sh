904b20b7e7b927d785e6aaa2b30faf5dcb9209c2
thelamer <ryankuba@gmail.com>
2024-08-06 19:19:18 -0400

    refresh arch-i3

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-PULSE_SCRIPT=/etc/xrdp/pulse/default.pa /startpulse.sh &
-setterm blank 0
-setterm powerdown 0 
+
+# Combine env
+/usr/bin/with-contenv /usr/bin/env | sed 's/^/export /g' > /defaults/env.sh
+source /defaults/env.sh
+rm /defaults/env.sh
+
+# Enable Nvidia GPU support if detected
+if which nvidia-smi; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Start Pulseaudio
+/startpulse.sh &
+
+# Launch DE
 /usr/bin/i3 > /dev/null 2>&1
